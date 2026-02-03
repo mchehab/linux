204d60831cf1279c0138f1d04d8bcbb58b74f80b
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2026-02-03 15:08:33 +0100

    docs: kernel-doc: add support to store output on a YAML file
    Add a command line parameter and library support to optionally
    store:
    - KdocItem intermediate format after parsing;
    - man pages output;
    - rst output.
    
    inside a YAML file.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- tools/lib/python/kdoc/kdoc_parser.py
+++ tools/lib/python/kdoc/kdoc_parser.py
@@ -258,12 +258,13 @@ class KernelDoc:
     #: String to write when a parameter is not described.
     undescribed = "-- undescribed --"
 
-    def __init__(self, config, fname, xforms):
+    def __init__(self, config, fname, xforms, store_src=False):
         """Initialize internal variables"""
 
         self.fname = fname
         self.config = config
         self.xforms = xforms
+        self.store_src = store_src
 
         # Initial state for the state machines
         self.state = state.NORMAL
@@ -273,6 +274,9 @@ class KernelDoc:
 
         # Place all potential outputs into an array
         self.entries = []
+
+        # When store_src is true, the kernel-doc source content is stored here
+        self.source = None
 
         #
         # We need Python 3.7 for its "dicts remember the insertion
@@ -1612,6 +1616,15 @@ class KernelDoc:
         state.DOCBLOCK:			process_docblock,
         }
 
+    def get_source(self):
+        """
+        Return the file content of the lines handled by kernel-doc at the
+        latest parse_kdoc() run.
+
+        Returns none if KernelDoc() was not initialized with store_src,
+        """
+        return self.source
+
     def parse_kdoc(self):
         """
         Open and process each line of a C source file.
@@ -1625,6 +1638,8 @@ class KernelDoc:
         prev = ""
         prev_ln = None
         export_table = set()
+        self.source = []
+        self.state = state.NORMAL
 
         try:
             with open(self.fname, "r", encoding="utf8",
@@ -1651,6 +1666,8 @@ class KernelDoc:
                                           ln, state.name[self.state],
                                           line)
 
+                    prev_state = self.state
+
                     # This is an optimization over the original script.
                     # There, when export_file was used for the same file,
                     # it was read twice. Here, we use the already-existing
@@ -1660,6 +1677,14 @@ class KernelDoc:
                        not self.process_export(export_table, line):
                         # Hand this line to the appropriate state handler
                         self.state_actions[self.state](self, ln, line)
+
+                    if self.store_src and prev_state != self.state or self.state != state.NORMAL:
+                        if self.state == state.NAME:
+                            # A "/**" was detected. Add a new source element
+                            self.source.append({"ln": ln, "data": line + "\n"})
+                        else:
+                            # Append to the existing one
+                            self.source[-1]["data"] += line + "\n"
 
             self.emit_unused_warnings()
 
