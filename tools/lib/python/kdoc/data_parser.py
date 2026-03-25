b67c626f2646059b47a746975a471e6e8239d329
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2026-03-25 10:15:10 +0100

    Revert "DEBUG"
    This reverts commit 06e747cc335398822e3b7a6e5ca069e9fe704514.

--- tools/lib/python/kdoc/data_parser.py
+++ tools/lib/python/kdoc/data_parser.py
@@ -130,7 +130,6 @@ class CDataParser:
             if cur_tok.level != start_level:
                 continue
 
-#            print(cur_tok)
 
             if cur_tok.kind == CToken.COMMA:
                 has_comma = True
@@ -175,7 +174,6 @@ class CDataParser:
         #
         # Pick remaining elements of the same type
         #
-        print(token_list[pos + 1:])
         if has_comma:
             type_name = ""
             for j in range(pos + 1, len(token_list)):
@@ -198,9 +196,6 @@ class CDataParser:
 
             if type_name:
                 type_list.append(type_name.strip())
-
-        print(name_list)
-        print(type_list)
 
         for i in range(0, len(name_list)):
             name = name_list[i]
